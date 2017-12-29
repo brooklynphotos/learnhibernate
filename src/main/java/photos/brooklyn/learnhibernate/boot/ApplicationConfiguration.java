66d77f28adf537484679d5037079673131c6a08e
Brooklyn Photos <guoneng.list@gmail.com>
2017-12-28 23:16:29 -0500

    cleaned up import

--- src/main/java/photos/brooklyn/learnhibernate/boot/ApplicationConfiguration.java
+++ src/main/java/photos/brooklyn/learnhibernate/boot/ApplicationConfiguration.java
@@ -1,9 +1,7 @@
 package photos.brooklyn.learnhibernate.boot;
 
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 
@@ -17,11 +15,10 @@ public class ApplicationConfiguration {
 
         // no need shutdown, EmbeddedDatabaseFactoryBean will take care of this
         EmbeddedDatabaseBuilder builder = new EmbeddedDatabaseBuilder();
-        EmbeddedDatabase db = builder
+        return builder
                 .setType(EmbeddedDatabaseType.HSQL) //.H2 or .DERBY
                 .addScript("db/sql/create-db.sql")
                 .addScript("db/sql/insert-data.sql")
                 .build();
-        return db;
     }
 }
